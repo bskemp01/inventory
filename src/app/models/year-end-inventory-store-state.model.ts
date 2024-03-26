3b50d081eafde5c162d27b9b75a71c0967194430
null <null>
2024-03-26 14:57:06 -0500

    stackblitz changes

--- src/app/models/year-end-inventory-store-state.model.ts
+++ src/app/models/year-end-inventory-store-state.model.ts
@@ -1,17 +1,7 @@
 import { SosCompletionReportModel } from './year-end-inventory-models/sosCompletionReportModel';
-import { SosTicketModel } from './year-end-inventory-models/sosTicketModel';
-import { SosZmmModel } from './year-end-inventory-models/sosZmmModel';
 import { URCompletionReportModel } from './year-end-inventory-models/uRCompletionReportModel';
-import { UnrestrictedTicketModel } from './year-end-inventory-models/unrestrictedTicketModel';
-import { UnrestrictedZmmModel } from './year-end-inventory-models/unrestrictedZmmModel';
 
 export interface YearEndInventoryStoreState {
-  lineItems: string[];
-  reportData: string[];
-  saleOrderData: SosZmmModel[];
-  searchedSalesOrder: SosTicketModel;
-  searchedUnrestrictedTicket: UnrestrictedTicketModel;
   sosCompletionReport: SosCompletionReportModel[];
-  unrestrictedRowData: UnrestrictedZmmModel;
   urCompletionReport: URCompletionReportModel[];
 }
